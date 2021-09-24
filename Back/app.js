c34b777022f8fefd23dc95b436425e428a367787
Christian Tellez <ch.tellez@hotmail.com>
2021-09-23 20:18:59 -0500

    Configurando servidor inicial express en app.js

--- Back/app.js
+++ Back/app.js
@@ -1,2 +1,14 @@
-console.log("hola mundo");
-console.log("probando nodemon");
+require("dotenv").config();
+
+//Configuración del servidor Web
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Iniciar el servidor
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Servidor express Aforo Web App funcionando en http://localhost:${port}`));
