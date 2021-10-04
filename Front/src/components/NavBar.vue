0c2728e5565774a08a30711448e173026d3b5a60
Christian Tellez <ch.tellez@hotmail.com>
2021-10-04 12:28:00 -0500

    ´ÂCreando el componente NavBar

---
+++ Front/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card>
+      <v-app-bar
+        app
+        abosolute
+        color="red"
+        elevate-on-scroll>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Aforo App</v-toolbar-title>
+      </v-app-bar>
+    </v-card>
+</template>
