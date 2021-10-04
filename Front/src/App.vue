b9de779ec59cf6e50a7905d38b69c08227030968
Christian Tellez <ch.tellez@hotmail.com>
2021-10-04 12:26:51 -0500

    Ajustando el App.vue por componentes

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,86 +1,28 @@
 <template>
   <v-app>
-  <v-card class="overflow-hidden">
-    <v-app-bar
-      absolute
-      dark
-      color="#D32F2F"
-      elevate-on-scroll
-      shrink-on-scroll
-      scroll-target="#scrolling-techniques-7"
-      fade-img-on-scroll
-      scroll-threshold="300"
-      
-      src="https://s1.eestatic.com/2020/06/01/cultura/teatro_real-cultura_494461818_152928685_1706x960.jpg"
-    >
-      
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Aforo App</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <v-sheet
-      id="scrolling-techniques-7"
-      class="overflow-y-auto"
-      max-height="600"
-    >
-      <v-container style="height: 1500px;">
-      </v-container>
-    </v-sheet>
-  </v-card>
+    <nav-bar></nav-bar>
+    <main-content></main-content>
+    <app-footer></app-footer>
   </v-app>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue'
+import MainContent from './components/MainContent.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
+  name: 'App',
+
   components: {
+    NavBar,
+    MainContent,
+    AppFooter
+
   },
+
   data: () => ({
-    drawer: false,
-    group: null
+    //
   }),
 };
 </script>
