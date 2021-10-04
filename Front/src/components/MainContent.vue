a2b55bd300be38e01b3b1ccf796d920c0ec9e6b7
Christian Tellez <ch.tellez@hotmail.com>
2021-10-04 12:28:47 -0500

    Creando el componente MainContent

---
+++ Front/src/components/MainContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-main app>
+    <v-container class="overflow-y-auto" id="main">
+      <home-content-component></home-content-component>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+
+import HomeContentComponent from './HomeContentComponent.vue';
+export default {
+  components: {
+    HomeContentComponent
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
